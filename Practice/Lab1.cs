71f9211ceab3f1d9b871523fd5c90faa97eedb92
EMatsonE <90651995+EMatsonE@users.noreply.github.com>
2022-04-25 18:10:41 -0500

    Add project files.

---
+++ Practice/Lab1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Lab1
+    {
+        public static void AreaAndPerimeter()
+        {
+            bool repeat = true;
+            do
+            {
+                GreetingInput();
+                Console.WriteLine("Sweet! Wanna redo the simulation?");
+                string redo = Console.ReadLine();
+                if (redo == "yes")
+                {
+                    repeat = true;
+                }
+                else
+                {
+                    repeat = false;
+                    Environment.Exit(0);
+                }
+            }
+            while (repeat == true);
+        }
+        static void GreetingInput()
+        {
+            Console.WriteLine("Hello! Are you ready for some math?");
+            Console.WriteLine("Enter Length");
+            float input1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Width");
+            float input2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your area is:");
+            Console.WriteLine(Area(input1, input2));
+
+            Console.WriteLine("Your parameter is:");
+            Console.WriteLine(Perimeter(input1, input2));
+        }
+        static float Area(float num1, float num2)
+        {
+            float total = num1 * num2;
+            return total;
+        }
+        static float Perimeter(float num1, float num2)
+        {
+            float total = (num1 * 2) + (num2 * 2);
+            return total;
+        }
+    }
+}
---
+++ Practice/Lab2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Lab2
+    {
+        public static void EvenOdd100s()
+        {
+            string name = Greeting();
+            bool repeat = true;
+            do
+            {
+                Begin(name);
+                Console.WriteLine("Would you like to go again?");
+                string redo = Console.ReadLine();
+                if (redo == "yes")
+                {
+                    repeat = true;
+                }
+                else
+                {
+                    repeat = false;
+                    Environment.Exit(0);
+                }
+            }
+            while (repeat == true);
+        }
+        static string Greeting()
+        {
+            Console.WriteLine("Hello! What is your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static void Begin(string name)
+        {
+            Console.WriteLine($"Please enter a number {name}");
+            float input = float.Parse(Console.ReadLine());
+
+            GoThrough(input, name);
+        }
+        static void GoThrough(float input, string name)
+        {
+            if (input < 1 || input > 100)
+            {
+                Console.WriteLine("Please put valid input");
+                Begin(name);
+            }
+            else
+            {
+                EvenOdds(input);
+                Ranges(input);
+            }
+        }
+        static void EvenOdds(float input)
+        {
+            if (input % 2 == 0)
+            {
+                Console.WriteLine($"{input} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is odd");
+            }
+        }
+        static void Ranges(float input)
+        {
+            if (input < 25)
+            {
+                Console.Write(" and under 25");
+            }
+            if (input < 60 && input > 26)
+            {
+                Console.WriteLine(" and it is between 25 and 60");
+            }
+            if (input > 60)
+            {
+                Console.WriteLine(" and it is bigger than 60");
+            }
+        }
+    }
+}
